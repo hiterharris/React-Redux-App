d84fd5cd8f5a77062fa326fb888b587f8c7d989f
Hiter Harris <hiterharris@comcast.net>
2020-01-23 16:56:29 -0500

    component structure, set up initial state in redux

---
+++ react-redux-app/src/components/RestaurantList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+import Restaurant from './Restaurant';
+
+const RestaurantList = () => {
+  return (
+    <div className="RestaurantList">
+      <Restaurant />
+    </div>
+  );
+}
+
+export default RestaurantList;
---
+++ react-redux-app/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    isLoading: false,
+    businessList: [],
+    error: ""
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
