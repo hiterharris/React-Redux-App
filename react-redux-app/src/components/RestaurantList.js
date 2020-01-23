75f12005da6ef4261177072955c535b084b41b8b
Hiter Harris <hiterharris@comcast.net>
2020-01-23 18:39:43 -0500

    added restaurant link

--- react-redux-app/src/components/RestaurantList.js
+++ react-redux-app/src/components/RestaurantList.js
@@ -10,17 +10,19 @@ const RestaurantList = props => {
             <div className="RestaurantList">
                 {props.restaurantList.splice(1).map((restaurant) => {
                     return (
-                        <div className='restaurant'>
-                            <div className='heading'>
-                                <h1>{restaurant.name}</h1>
-                                <p>{restaurant.categories[0].title}</p>
-                                <div className='rating'>
-                                    <p>Rating: {restaurant.rating}</p>
-                                    <p>(    {restaurant.review_count} reviews)</p>
+                        <a className='restaurant' href={restaurant.url} target='_blank' rel="noopener noreferrer">
+                            <div>
+                                <div className='heading'>
+                                    <h1>{restaurant.name}</h1>
+                                    <p>{restaurant.categories[0].title}</p>
+                                    <div className='rating'>
+                                        <p>Rating: {restaurant.rating}</p>
+                                        <p>(    {restaurant.review_count} reviews)</p>
+                                    </div>
                                 </div>
+                                <img src={restaurant.image_url} />
                             </div>
-                            <img src={restaurant.image_url} />
-                        </div>
+                        </a>
                     );
                 })}
             </div>
