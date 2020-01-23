1e612d6fd239ed62bd99c7758bb8b8e36dc25139
Hiter Harris <hiterharris@comcast.net>
2020-01-23 18:23:54 -0500

    adding styles

--- react-redux-app/src/components/RestaurantList.js
+++ react-redux-app/src/components/RestaurantList.js
@@ -6,27 +6,20 @@ import {buttonClicked, fetchRestaurantList} from '../actions';
 
 const RestaurantList = props => {
     props.fetchRestaurantList();
-    const restaurant = props.restaurantList.map((restaurant) => {
-        return (
-            <div>
-                <h1>{restaurant.name}</h1>
-                <p>{restaurant.categories[0].title}</p>
-                <p>{restaurant.rating}</p>
-                <p>{restaurant.reviews}</p>
-                <img src={restaurant.image_url} />
-            </div>
-        );
-    });
 
     return (
             <div className="RestaurantList">
                 {props.restaurantList.splice(1).map((restaurant) => {
                     return (
-                        <div>
-                            <h1>{restaurant.name}</h1>
-                            <p>{restaurant.categories[0].title}</p>
-                            <p>{restaurant.rating}</p>
-                            <p>{restaurant.reviews}</p>
+                        <div className='restaurant'>
+                            <div className='heading'>
+                                <h1>{restaurant.name}</h1>
+                                <p>{restaurant.categories[0].title}</p>
+                                <div className='rating'>
+                                    <p>Rating: {restaurant.rating}</p>
+                                    <p>({restaurant.review_count} reviews)</p>
+                                </div>
+                            </div>
                             <img src={restaurant.image_url} />
                         </div>
                     );
