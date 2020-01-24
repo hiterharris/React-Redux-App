fbddb3937f26c9b8898ba74cfcc28279ec78a00e
Hiter Harris <hiterharris@comcast.net>
2020-01-23 19:02:03 -0500

    added yelp logo

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import yelp from './images/yelp.png';
 
 import RestaurantList from './components/RestaurantList';
 
 const App = () => {
   return (
     <div className="App">
+      <img src={yelp} />
       <RestaurantList />
     </div>
   );
